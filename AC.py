5242e15e8a08a383bd04908cb6a84fd151211a23
agathelhm <agathe.lhermite@live.fr>
2024-02-15 10:59:35 +0100

    Modification de AC pour n'ajouter qu'une fois les contraintes impactées

--- AC.py
+++ AC.py
@@ -5,14 +5,11 @@ def AC(variables, contraintes):
         nonlocal valeurs
         nonlocal c1
         nonlocal c2
+        nonlocal ajout_contraintes
         for b in variables[c2]:
             if (a, b) in valeurs:
                 return True
-        for indices, _ in contraintes.items(): # ajout des contraintes de la forme (c3, c1)
-                if c1 == indices[0] and c2 not in indices:
-                    aTester.append((indices[1], c1))
-                elif c1 == indices[1] and c2 not in indices:
-                    aTester.append((indices[0], c1))
+        ajout_contraintes = True
         return False
     
     aTester = []
@@ -25,5 +22,12 @@ def AC(variables, contraintes):
             valeurs = contraintes[(c1, c2)]
         else :
             valeurs = contraintes[(c2, c1)]
-        variables[c1] = [a for a in variables[c1] if LocAC(a)]           
+        ajout_contraintes = False
+        variables[c1] = [a for a in variables[c1] if LocAC(a)]  
+        if ajout_contraintes:
+            for indices, _ in contraintes.items(): # ajout des contraintes de la forme (c3, c1)
+                if c1 == indices[0] and c2 not in indices:
+                    aTester.append((indices[1], c1))
+                elif c1 == indices[1] and c2 not in indices:
+                    aTester.append((indices[0], c1))         
     return(variables, contraintes)
